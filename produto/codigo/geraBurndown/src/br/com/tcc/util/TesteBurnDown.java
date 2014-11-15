828f577f09e618cc5e8476e5400fa10189bdd3e0
henrique borges <henriquemborges8@gmail.com>
2014-11-15 17:25:27 -0200

    Ajustes de dependencias do projeto e criacao classe de testes.

---
+++ produto/codigo/geraBurndown/src/br/com/tcc/util/TesteBurnDown.java
@@ -0,0 +1,14 @@
+package br.com.tcc.util;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TesteBurnDown extends TestCase{
+	
+	@Test
+	public void testaAlgo(){
+		int valor = 1;
+		assertEquals(1, valor);
+	}
+}
