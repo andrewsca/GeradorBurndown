a286967a87dd3965313614f348ffbacac105c8e5
Cristiana <cristina.sant.silva@gmail.com>
2014-11-19 17:38:27 -0200

    Complementando Testes
    Complementando métodos de testes.

--- produto/codigo/geraBurndown/src/br/com/tcc/util/TesteBurnDown.java
+++ produto/codigo/geraBurndown/src/br/com/tcc/util/TesteBurnDown.java
@@ -13,46 +13,47 @@ public class TesteBurnDown extends TestCase{
 	
 	@Test
 	public void testaSprintDeveTer170HorasTotais() throws ParseException{
-		Sprint sprint = Main.criaSprint();
 		
+		Sprint sprint = Main.criaSprint();		
 		assertEquals(170.0, sprint.getTotalHoras());
 	}
 	
 	@Test
 	public void testaSprintDeveTer7Dias() throws ParseException{
-		Sprint sprint = Main.criaSprint();
 		
+		Sprint sprint = Main.criaSprint();		
 		assertEquals(7, sprint.getQtdeDias());
 	}
 	
 	@Test
 	public void testaDeveRetornarDataNula() throws ParseException{
+		
 		Date date = DataUtil.converteStringParaDate("");
 		assertNull("mensagem", date);
 	}
 	
 	@Test(expected=ParseException.class)
 	public void testaDeveRetornarExcecao() throws ParseException{
+		
 		@SuppressWarnings("unused")
 		Date date = DataUtil.converteStringParaDate("10/45/1991");
 	}
 	
 	@Test
 	public void testaSprintDataInicioNula() throws ParseException {
-		Sprint sprint = new Sprint();
 		
+		Sprint sprint = new Sprint();		
 		assertEquals(null, sprint.getDtInicio());
 	}
 	
 	@Test
 	public void testaSprintDataFim01Dezembro2014() throws ParseException {
-		Sprint sprint = new Sprint();
+		
+		Sprint sprint = new Sprint();	
 		
 		@SuppressWarnings("deprecation")
-		Date data = new Date(2014,12,01);
-		
-		sprint.setDtFim(data);
-		
+		Date data = new Date(2014,12,01);		
+		sprint.setDtFim(data);		
 		assertEquals(data, sprint.getDtFim());
 	}
 	
