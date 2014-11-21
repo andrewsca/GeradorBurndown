d91264b4b1af90e32a7107f5f7c754e617cf8034
henrique borges <henriquemborges8@gmail.com>
2014-11-20 23:47:19 -0200

    Configuracao de dependencias e criacao de um teste que falha.

--- GeradorBurnUp/src/TesteBurnUp.java
+++ GeradorBurnUp/src/TesteBurnUp.java
@@ -1,4 +1,21 @@
+import org.junit.Test;
 
-public class TesteBurnUp {
+import junit.framework.TestCase;
 
+/**
+ * TDD Gerador BurnUp
+ * @author Jenifer Henrique
+ *
+ */
+public class TesteBurnUp extends TestCase{
+	
+	@Test
+	public void testaSprintDeveTer10Dias(){
+		Sprint sprint = new Sprint();
+		sprint.setQtdeDias(10);
+		
+		assertEquals(10, sprint.getQtdeDias());
+	}
+	
+	
 }
