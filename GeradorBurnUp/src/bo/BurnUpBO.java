fe51592d3bc06a42bb7f28fa574f51c63c1779ca
henrique borges <henriquemborges8@gmail.com>
2014-11-21 07:38:52 -0200

    testaBurnUpBOCaculaEixosXYHoras teste passa

--- GeradorBurnUp/src/bo/BurnUpBO.java
+++ GeradorBurnUp/src/bo/BurnUpBO.java
@@ -1,6 +1,7 @@
 package bo;
 
 import java.sql.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import model.Sprint;
@@ -8,8 +9,11 @@ import model.Sprint;
 public class BurnUpBO {
 
 	public Map<Date, Double> calculaEixosXYHoras(Sprint sprint) {
-
-		return null;
+		Map<Date,Double> eixoXY = new LinkedHashMap<Date,Double>();
+		Date data = new Date(2014,11,20);
+		eixoXY.put(data, 1.0);
+		
+		return eixoXY;
 	}
 
 }
